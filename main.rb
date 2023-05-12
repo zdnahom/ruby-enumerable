f241edb045488d169ef91133aafa931205b0601a
abenaTenkorang <abenagy16@gmail.com>
2023-05-12 09:18:31 +0000

    Fix linter errors

--- main.rb
+++ main.rb
@@ -1,19 +1,19 @@
 require_relative 'my_enumerable'
 
 class MyList
+  include MyEnumerable
 
-    include MyEnumerable
+  def initialize(list)
+    @list = list
+  end
 
-    def initialize(list)
-        @list=list
-    end
-    
-    def each
-        @list.each{|element| yield element}
-    end
-
+  def each(&block)
+    @list.each(&block)
+  end
 end
 
-list = MyList.new([1,2,3,4])
-list.each{|e| puts e}
-list.all?{|e| e < 5}
+list = MyList.new([1, 2, 3, 4])
+list.each { |e| puts e }
+list.all? { |e| e < 5 }
+list.any? { |e| e < 5 }
+list.filter(&:even?)
--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,35 +1,31 @@
 module MyEnumerable
-    def all?
-        each do |num|
-           unless yield num
-            puts false
-            return
-           end
-        end
-        puts true
-        return
-    end
-
-    def any?
-        each do |num|
-           if yield num
-            puts true
-            return
-           end
-        end
+  def all?
+    each do |num|
+      unless yield num
         puts false
         return
+      end
     end
+    puts true
+    nil
+  end
 
-    def filter
-        filtered=[]
-        each do |num|
-           if yield num
-            filtered.push(num)
-           end
-        end
-       puts filtered.inspect
+  def any?
+    each do |num|
+      if yield num
+        puts true
+        return
+      end
     end
+    puts false
+    nil
+  end
 
-
+  def filter
+    filtered = []
+    each do |num|
+      filtered.push(num) if yield num
+    end
+    puts filtered.inspect
+  end
 end
